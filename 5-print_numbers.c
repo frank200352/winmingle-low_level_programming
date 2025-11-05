6d06f2d304053526f5d97995f2ba2f53cf4daf48
frank200352 <franklin.monday@yahoo.com>
2025-11-05 01:48:53 +0100

    Add program that prints single-digit numbers

---
+++ 5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Author: Adubiaro Franklin Monday
+ * Program: WinMingle Community C Training
+ * Description: program to print all single digit number of base 10, starting from 0
+ */
+
+int main(void)
+
+{ 
+	int num;
+
+	for ( num = 0; num < 10; num++)
+	{ 
+		printf("%d", num);
+	}
+
+	printf("\n");
+
+	return 0;
+}
