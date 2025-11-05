a8c5eda54728bc54dc9d83e2f3aa751c49d2b0e8
frank200352 <franklin.monday@yahoo.com>
2025-11-05 01:48:43 +0100

    Add program that prints lowercase alphabet

---
+++ 2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * Author: Adubiaro Franklin Monday
+ * Program: WinMingle Community C Training
+ * Description: To print alphabet in lowercase
+ */
+
+int main(void)
+
+{ 
+	char c;
+
+	for ( c = 'a'; c <= 'z'; c++)
+
+	{ 
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
